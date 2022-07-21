39b5ac178c1f1db82ea0076c8ef286da25742902
Grodawah <tomnoamadjedj@gmail.com>
2022-07-21 15:45:34 +0200

    1.0.1 (step 6 done)

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,62 @@
+window.addEventListener('DOMContentLoaded', async (event) => {
+   
+// -------------get the ID-----"idKanap"----------------
+    const productID = window.location.search;
+    const urlParams = new URLSearchParams(productID);
+    const idKanap = urlParams.get('id');
 
+    console.log(idKanap);
+
+// ------------then request API with the correct ID--------------
+let data;
+    try {
+        const APICallRes = await fetch("http://localhost:3000/api/products/"+ idKanap);
+        data = await APICallRes.json();
+        if (APICallRes.status == 404) {
+            throw "API endpoint not found";
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    // console.log(data);
+
+//----------------------- MODIF ----------------
+
+    let articleIMG = document.querySelector('#ID_img');
+
+        let imageKanap = document.createElement('img');
+            imageKanap.src = data.imageUrl;
+            imageKanap.alt = data.altTxt;
+
+    articleIMG.appendChild(imageKanap); 
+
+    console.log(articleIMG);
+    console.log(imageKanap);
+        
+    
+
+    let titleKanap = document.querySelector('#title');
+
+        titleKanap.innerHTML = data.name;
+
+    let priceKanap = document.querySelector('#price');
+
+        priceKanap.innerHTML = data.price;
+
+    let descriptionKanap = document.querySelector('#description');
+
+        descriptionKanap.innerHTML = data.description;
+
+    const optionColors = document.querySelector('#colors');
+    let tabColors = data.colors;
+
+        for ( i=0 ; i < tabColors.length ; i++ ) {
+            let opt = document.createElement('option');
+            opt.value = tabColors[i];
+            opt.innerHTML = tabColors[i];
+
+            optionColors.appendChild(opt);
+        }
+
+    // console.log(tabColors[1]);   
+});
--- front/js/script.js
+++ front/js/script.js
@@ -14,4 +14,47 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     // display products from the response obtained from the API
     console.log(data);
 
+
+    const daddy = document.querySelector('#daddyCool');
+        daddy.classList.add('limitedWidthBlock');
+
+    for ( i=0 ; i < data.length; i++ ) {
+        console.log(data[i]);
+
+        let newKanap = document.createElement('section');
+            newKanap.classList.add('items');
+
+        let newLinkKanap = document.createElement('a');
+            newLinkKanap.href = "./product.html?id=" + data[i]._id;
+
+        let newArticle = document.createElement('article');
+        
+        let newImageKanap = document.createElement('img');
+            newImageKanap.src = data[i].imageUrl;
+            newImageKanap.alt = data[i].altTxt;
+        
+        let newNameKanap = document.createElement('h3');
+            newNameKanap.classList.add('productName');
+            newNameKanap.innerText = data[i].name;
+
+        let newDescriptionKanap = document.createElement('p');    
+            newDescriptionKanap.classList.add('productDescription');
+            newDescriptionKanap.innerText = data[i].description;
+
+        newKanap.append(newLinkKanap);
+        newLinkKanap.append(newArticle);
+        newArticle.append(newImageKanap);
+        newArticle.append(newNameKanap);
+        newArticle.append(newDescriptionKanap);
+        
+        daddy.append(newKanap);       
+    }
 });
+
+    // linkKanap.innerHTML = data[i].linkKanap;    
+    // nameKanap.innerHTML = data[i].name;
+    // descriptionKanap.innerHTML = data[i].description;
+    // imageKanap.src = data[i].imageUrl;
+    // imageKanap.alt = data[i].altTxt;
+  
+    // console.log(item);
